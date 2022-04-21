05257fe7505701ceb8f830626e681cfb0f2aa3cd
0x0a0d <vandug23@gmail.com>
2022-04-21 14:01:14 +0700

    d.ts more flexible

--- src/array-same-structure.ts
+++ src/array-same-structure.ts
@@ -1,12 +1,12 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export function compact<HEADER = any, DATA = any>(arr: any[], defaultValue?: any): [headers: HEADER[], ...data: DATA[][]] {
+export function compact<HEADER = any, DATA extends Array<any> = any[]>(arr: any[], defaultValue?: any): [headers: HEADER[], ...data: DATA[]] {
   const headers = []
 
   const data = arr.map(item => {
     Object.keys(item).forEach(key => headers.includes(key) || headers.push(key))
     return headers.map(key => item[key] ?? defaultValue)
   })
-  return [headers, ...data]
+  return [headers].concat(data) as any
 }
 
 declare function filterInput(input: any): boolean
